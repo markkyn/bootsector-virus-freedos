41005ca2644f834ce7c1c33993b0a4afc94911fe
Marcos Gabriel da Silva Santos <marcos.santos@lais.huol.ufrn.br>
2023-09-21 22:04:37 -0300

    fix: Ajeitando Memory Map e seu print (10h)

--- src/loader.asm
+++ src/loader.asm
@@ -45,9 +45,18 @@ start:
     mov al, 0x01 ; Write Mode = True
     mov bx, 0x0a
     xor dx, dx
-    xor dh, 0x01
+    mov dh, 0x01
     mov bp, longmode_message ; Ponteiro de String 
     mov cx, longmode_len     ; Numero de Caracteres
+    int 10h
+
+    mov ah, 0x13 ; Write string (EGA+, meaning PC AT minimum) 
+    mov al, 0x01 ; Write Mode = True
+    mov bx, 0x0a
+    xor dx, dx
+    mov dh, 0x02
+    mov bp, memorymap_msg ; Ponteiro de String 
+    mov cx, memorymap_len     ; Numero de Caracteres
     int 10h
 
 ; Carregamento do Kernel File
@@ -62,42 +71,39 @@ Kernel:
 
     mov dl, [ DriveId ]
     mov ah, 0x42
-    int 13h
+    int 0x13
     jc ReadError
-
-    mov dl, [ DriveId ]  
-    jmp 0x1000
 
 ; Loop sobre o Mapa de Memoria
 MermoryMap: ; Int 15h
-    mov eax, 0xE820
+    mov eax, 0xe820
     mov edx, 0x534d4150
     mov ecx, 20
     mov edi, 0x9000
     xor ebx, ebx
-    int 15h
+    int 0x15
     jc NotSupport
-
+    
 MemoryMapLoop:
-    add edi, 20
-    mov eax, 0xE820
-    mov edx, 0x534d4150
-    mov ecx, 20
-    int 15h
+    add edi,20
+    mov eax,0xe820
+    mov edx,0x534d4150
+    mov ecx,20
+    int 0x15
     jc MemoryMapDone
 
-    test ebx, ebx
+    test ebx,ebx
     jnz MemoryMapLoop
 
 MemoryMapDone:
-   ; Print("MemoryMap concluido")
+    
     mov ah, 0x13 ; Write string (EGA+, meaning PC AT minimum) 
     mov al, 0x01 ; Write Mode = True
     mov bx, 0x0a
     xor dx, dx
     mov dh, 0x02
     mov bp, memorymap_msg ; Ponteiro de String 
-    mov cx, memorymap_len ; Numero de Caracteres
+    mov cx, memorymap_len     ; Numero de Caracteres
     int 10h
 
 ReadError:
@@ -116,6 +122,6 @@ loader_len: equ $-loader_message
 longmode_message: db "Long Mode Suportado!", 0
 longmode_len: equ $-longmode_message
 
-memorymap_msg: db "Memory Map concluído!"
-memorymap_len: db "Memory Map concluído!"
+memorymap_msg: db "Memory Map concluido!", 0
+memorymap_len: equ $-memorymap_msg
 
