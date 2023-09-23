3f8a7fced0ade1504f2aee153599aab60e35658e
Marcos Gabriel da Silva Santos <marcos.santos@lais.huol.ufrn.br>
2023-09-23 01:36:07 -0300

    build: montagem e inclusao de kernel file

--- build_hw.sh
+++ build_hw.sh
@@ -1,5 +1,7 @@
 rm -f boot.img.lock
 nasm -f bin ./src/boot.asm -o ./bin/boot.bin
 nasm -f bin ./src/loader.asm -o ./bin/loader.bin
+nasm -f bin ./src/kernel.asm -o ./bin/kernel.bin
 dd if=./bin/boot.bin of=boot.img bs=512 count=1 conv=notrunc
 dd if=./bin/loader.bin of=boot.img bs=512 count=5 seek=1 conv=notrunc
+dd if=./bin/kernel.bin of=boot.img bs=512 count=100 seek=6 conv=notrunc
